0eb4a97d31c66dee3f83bce0929c916281f213be
Karlsooon <karakatalmasova2@gmail.com>
2024-02-17 05:16:20 +0600

    first commit

---
+++ src/test/java/com/karakat/spring/Canteen/CanteenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karakat.spring.Canteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
