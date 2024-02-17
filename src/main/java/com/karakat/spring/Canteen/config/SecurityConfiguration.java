e56e74ee8d6134a9048129829a15bb9a813eb4ef
Karlsooon <karakatalmasova2@gmail.com>
2024-02-17 14:27:02 +0600

    integrating keycloak

---
+++ src/main/java/com/karakat/spring/Canteen/config/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.karakat.spring.Canteen.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+}
