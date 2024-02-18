d5219b102effe927799173bba208e08ded4bc5f7
Karlsooon <karakatalmasova2@gmail.com>
2024-02-19 00:18:29 +0600

    fixed keycloak

--- src/main/java/com/karakat/spring/Canteen/CanteenApplication.java
+++ src/main/java/com/karakat/spring/Canteen/CanteenApplication.java
@@ -2,6 +2,22 @@ package com.karakat.spring.Canteen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CanteenApplication {
@@ -9,5 +25,6 @@ public class CanteenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CanteenApplication.class, args);
 	}
+
 
 }
