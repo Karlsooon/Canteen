7ddf8db37494b8ce202d068eae1fb1fa57198a2c
Karlsooon <karakatalmasova2@gmail.com>
2024-02-19 00:43:21 +0600

    commented bags annotation hasRole

--- src/main/java/com/karakat/spring/Canteen/controller/DishController.java
+++ src/main/java/com/karakat/spring/Canteen/controller/DishController.java
@@ -3,6 +3,7 @@ package com.karakat.spring.Canteen.controller;
 import com.karakat.spring.Canteen.dto.DishDto;
 import com.karakat.spring.Canteen.service.DishService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +16,20 @@ public class DishController {
     private final DishService dishService;
 
     @GetMapping("/all")
+//    @PreAuthorize("hasRole('client_admin')")
     public List<DishDto> findAll(){
         return dishService.findAll();
     }
 
     @GetMapping("/{id}")
+//    @PreAuthorize("hasRole('client_admin')")
     public DishDto findById(@PathVariable Long id){
        return dishService.findById(id);
 
     }
 
     @PostMapping("/create")
+//    @PreAuthorize("hasRole('client_admin')")
     public void createDish(@RequestBody DishDto dishDto){
         dishService.save(dishDto);
 
