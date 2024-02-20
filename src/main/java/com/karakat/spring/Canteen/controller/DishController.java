a25bc74244814d7be3ab8996ddbf928aff3e5bb5
Karlsooon <karakatalmasova2@gmail.com>
2024-02-20 12:23:24 +0600

    updated

--- src/main/java/com/karakat/spring/Canteen/controller/DishController.java
+++ src/main/java/com/karakat/spring/Canteen/controller/DishController.java
@@ -21,20 +21,17 @@ public class DishController {
     private final DishService dishService;
 
     @GetMapping("/all")
-//    @PreAuthorize("hasRole('client_admin')")
     public List<DishDto> findAll(){
         return dishService.findAll();
     }
 
     @GetMapping("/{id}")
-//    @PreAuthorize("hasRole('client_admin')")
     public DishDto findById(@PathVariable Long id){
        return dishService.findById(id);
 
     }
 
     @PostMapping(value = "/create", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
-    //    @PreAuthorize("hasRole('client_admin')")
     public ResponseEntity<DishDto> createDish(@ModelAttribute DishDto dishDto, @RequestParam("image") MultipartFile imageFile) throws IOException {
         return ResponseEntity.status(HttpStatus.CREATED).body(dishService.save(dishDto, imageFile));
     }
