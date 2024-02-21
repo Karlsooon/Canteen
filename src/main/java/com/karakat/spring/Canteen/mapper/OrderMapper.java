88f82ff8627b2dfc29fcaf014c02ef9307c42d18
Karlsooon <karakatalmasova2@gmail.com>
2024-02-21 17:52:33 +0600

    fix order mapper

--- src/main/java/com/karakat/spring/Canteen/mapper/OrderMapper.java
+++ src/main/java/com/karakat/spring/Canteen/mapper/OrderMapper.java
@@ -1,9 +1,41 @@
 package com.karakat.spring.Canteen.mapper;
 
 import com.karakat.spring.Canteen.dto.OrderDto;
+import com.karakat.spring.Canteen.model.Dish;
 import com.karakat.spring.Canteen.model.Orders;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "Spring")
-public interface OrderMapper extends BaseMapper<Orders, OrderDto>{
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper extends BaseMapper<Orders, OrderDto> {
+
+    @Override
+    @Mapping(source = "dishList", target = "dishIds")
+    OrderDto toDto(Orders entity);
+
+    default List<Long> mapDishListToIds(List<Dish> dishList) {
+        return dishList.stream()
+                .map(Dish::getId)
+                .collect(Collectors.toList());
+    }
 }
+
+
+
+
